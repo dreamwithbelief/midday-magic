a6e89135376517750866470d54115ea21619e3bd
Brandon G.A <brandonkeoni@gmail.com>
2014-11-08 19:54:24 -0800

    Initial Commit.  Starting again with CodeIgniter.

--- mvc/app/functions/sanitize.php
+++
@@ -1,5 +0,0 @@
-<?php
-function esc( $string )
-{
-    return htmlentities( $string, ENT_QUOTES, 'UTF-8' );
-}
--- mvc/app/views/admin/login.php
+++
@@ -1,9 +0,0 @@
-<h3><?= Session::get_flash('login'); ?></h3>
-<form action="login" method="post">
-    <label for="username">Username:</label>
-    <input type="text" id="username" name="username" />
-    <label for="password">Password:</label>
-    <input type="password" id="password" name="password" />
-    <input type="hidden" name="token" value="<?= Token::generate(); ?>" />
-    <input type="submit" id="submit" name="submit" value="Log In" />
-</form>
