055d7ccabcf966a24b668ae1cff883204dbcdafc
Interactive 2 <keenan@Keenans-iMac.local>
2014-11-19 16:56:07 -0800

    Copied views from page to use in admin. Need CKEditor or another WYSIWYG editor for content pages.

---
+++ application/views/admin/about.php
@@ -0,0 +1,21 @@
+<div class="container">
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2 content-block-lg">
+            <h1 class="text-center">About SpeakOut!</h1>
+            <div class="text-justify">
+             <p>   <img src="http://placehold.it/400x500" alt="Image" class="pull-right inner-sm">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis sit amet efficitur quam. Ut at consectetur urna, ac placerat neque. In interdum ultricies congue. Pellentesque sodales neque rutrum malesuada cursus. Praesent sit amet nibh ut ex lacinia pulvinar quis eget neque. In congue blandit dui vitae auctor. Sed ac est libero. Nullam urna mauris, faucibus ac massa non, malesuada porta neque. Vestibulum finibus blandit est, id pharetra lorem tincidunt ut. Aliquam blandit auctor velit, eget euismod metus. Nam dignissim volutpat mauris, vel cursus nulla iaculis in. Vivamus laoreet justo nisi, eu aliquet odio posuere quis. Proin vehicula dui quis mi facilisis venenatis. Etiam id augue ligula. Donec ac dictum tellus.</p>
+
+            <p> Vivamus tempor vehicula dignissim. Suspendisse elit sem, aliquam consectetur eleifend quis, facilisis ut lectus. Nulla nisi dolor, commodo vel hendrerit quis, blandit nec sapien. Vestibulum a eros nisl. Nunc non est a mi fermentum facilisis sit amet malesuada risus. Morbi elementum odio urna, in vehicula arcu semper sed. Suspendisse potenti. Integer lobortis erat ac erat efficitur, ac luctus sapien pellentesque. Proin dictum massa nibh, eu convallis mauris malesuada at. Nullam risus risus, tincidunt eget metus ac, ornare viverra nunc. Praesent in pretium augue, non maximus arcu. Fusce porttitor urna eget scelerisque tincidunt. Sed pellentesque, tellus sed rutrum porta, magna quam aliquam risus, nec sagittis libero leo id augue. Nullam ac quam nulla.</p>
+
+            <p> Phasellus vitae ligula mauris. Praesent vitae pellentesque tellus, vel venenatis turpis. Cras cursus mauris eu tortor rhoncus, id commodo felis efficitur. Vestibulum scelerisque sagittis pretium. Fusce facilisis, lectus ut euismod mattis, felis risus vestibulum enim, in euismod purus ante fringilla purus. Ut ultricies tempor suscipit. Morbi efficitur justo id mauris luctus bibendum. Aenean lorem dui, eleifend ut elementum at, tincidunt fermentum massa. In ullamcorper massa sit amet tellus consectetur faucibus. Fusce tempor velit fermentum urna consequat, ut tempor arcu sollicitudin. Morbi a augue dictum, maximus erat sollicitudin, sodales ipsum.</p>
+
+            <p> Sed a efficitur magna. Maecenas sed neque eget diam blandit finibus. Maecenas auctor lacinia arcu nec hendrerit. Nulla auctor posuere purus, quis tempor diam congue sit amet. Ut convallis ipsum nec venenatis ornare. Pellentesque metus justo, congue lacinia fringilla ac, iaculis vel orci. Curabitur tempus nisl quis ultrices mollis. Aliquam et dignissim arcu, vitae sodales eros. Fusce iaculis metus eros, vel porttitor nisi molestie quis. Quisque non metus eget lectus ultricies porttitor vitae a ipsum. Etiam at ex eu eros pretium elementum. Sed facilisis dolor feugiat, luctus augue non, luctus augue. Vestibulum rhoncus arcu ac vehicula mattis.</p>
+            </div>
+        </div>
+        <div class="col-md-8 col-md-offset-2 content-block-lg">
+            <form action="/admin/about" method="post">
+                    <!-- need ckeditor -->
+            </form>
+        </div>
+    </div>
+</div>
